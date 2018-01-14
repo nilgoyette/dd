694328ed6a645319bf4a3f9adcdbe248582047df
Nil Goyette <nil.goyette@gmail.com>
2018-01-14 15:20:52 -0500

    Abilities know their related skills

--- src/ability.rs
+++ src/ability.rs
@@ -1,4 +1,6 @@
 
+use Skill;
+
 #[derive(Clone, Copy, PartialEq)]
 pub enum Ability {
     Strength,
@@ -7,6 +9,32 @@ pub enum Ability {
     Intelligence,
     Wisdom,
     Charisma
+}
+
+impl Ability {
+    pub fn skills(&self) -> Vec<Skill> {
+        match *self {
+            Ability::Strength => vec![Skill::Athletics],
+            Ability::Dexterity => vec![Skill::Acrobatics,
+                                       Skill::SleightOfHand,
+                                       Skill::Stealth],
+            Ability::Constitution => vec![],
+            Ability::Intelligence => vec![Skill::Arcana,
+                                          Skill::History,
+                                          Skill::Investigation,
+                                          Skill::Nature,
+                                          Skill::Religion],
+            Ability::Wisdom => vec![Skill::AnimalHandling,
+                                    Skill::Insight,
+                                    Skill::Medicine,
+                                    Skill::Perception,
+                                    Skill::Survival],
+            Ability::Charisma => vec![Skill::Deception,
+                                      Skill::Intimidation,
+                                      Skill::Performance,
+                                      Skill::Persuasion]
+        }
+    }
 }
 
 pub trait Modifier {
